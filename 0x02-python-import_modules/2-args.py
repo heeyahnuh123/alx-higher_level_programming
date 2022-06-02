1033b0890b2774e6d374f5e50d4dbf903b439b45
heeyahnuh123 <heeyahnuh@gmail.com>
2022-06-02 15:37:43 -0700

    argument

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+def check_args(argv):
+    length = len(argv)
+    i = 1
+
+    if length > 2:
+        print("{:d} arguments:".format(length - 1))
+    elif length == 2:
+        print("{:d} argument:".format(length - 1))
+    else:
+        print("{:d} arguments.".format(length - 1))
+        return
+
+    while i < length:
+        print("{:d}: {:s}".format(i, argv[i]))
+        i += 1
+
+if __name__ == "__main__":
+
+    import sys
+    check_args(sys.argv)
