c6b9b529af9d77f8d19d8f96b39582f5ebfacae6
heeyahnuh123 <heeyahnuh@gmail.com>
2022-06-02 16:12:07 -0700

    initial commit

---
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+import hidden_4
+def unhide():
+    for files in dir(hidden_4):
+        if files[:2] != "__":
+            print("{:s}".format(files))
+
+if __name__ == "__main__":
+    unhide()
