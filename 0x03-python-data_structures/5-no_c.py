a84c7e7fb1c7c0bf29b39c595801c11f9e653cdd
heeyahnuh123 <heeyahnuh@gmail.com>
2022-06-06 17:05:30 -0700

    initial commit

---
+++ 0x03-python-data_structures/5-no_c.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+
+def no_c(my_string):
+    st = ''
+    for i in range(len(my_string)):
+        if my_string[i] != 'C' and my_string[i] != 'c':
+            st += my_string[i]
+    return st
